0ccba9fd3eba391bb9fb6e83e5caea489e7ebd45
Monk <sabintheworld@gmail.com>
2017-09-10 00:07:36 -0700

    Last-Minute Friend Page Changes

--- src/pages/friends/friends-service.ts
+++ src/pages/friends/friends-service.ts
@@ -10,39 +10,47 @@ export class FriendsService extends BaseService {
             {
                 id: 1,
                 name: "sabin",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color: 'blue'
             },
             {
                 id: 2,
                 name: "andrew",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color:'red'
             },{
                 id: 3,
                 name: "eli",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color:'purple'
             },
             {
                 id: 4,
                 name: "cory",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color:'yellow'
             },{
                 id: 5,
                 name: "paul",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color: 'orange'
             },
             {
                 id: 6,
                 name: "aaron",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color: 'green'
             },{
                 id: 7,
                 name: "alex",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color: 'white'
             },
             {
                 id: 8,
                 name: "colby",
-                image: 'http://placehold.it/100x100'
+                image: 'http://placehold.it/100x100',
+                color: 'pink'
             }
         ]
     }
