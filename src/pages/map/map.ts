66d905a1e52382df25bb4c1bf6205da8d06214fe
Monk <sabintheworld@gmail.com>
2017-09-13 23:29:16 -0700

    Marker Page Popups

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,49 +1,32 @@
-import { Component, NgZone, ChangeDetectorRef} from '@angular/core';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions } from '@ionic-native/google-maps';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavInterceptor } from '../../shared/services/nav-interceptor';
-
+declare var google;
 @Component({
     selector: 'map-page',
     templateUrl: 'map.html'
 })
 export class MapPage {
-    map: GoogleMap;
-    mapElement: HTMLElement;
-    changeDetectorRefs:ChangeDetectorRef[] = [];
-    constructor( private googleMaps: GoogleMaps, public _navCtrl: NavInterceptor, public zone: NgZone, public ChangeDetectorRef: ChangeDetectorRef ) {}
-
-    ionViewDidLoad() {
-        
-        setTimeout(() => {
-            this.initMap();
-        }, 1000);
-    }
+    @ViewChild('map') mapElement: ElementRef;
+    map: any;
+    constructor( public _navCtrl: NavInterceptor ) {}
     
-    initMap() {
-        let mapOptions: GoogleMapOptions = {
-            camera: {
-              target: {
-                lat: 43.0741904,
-                lng: -89.3809802
-              },
-              zoom: 18,
-              tilt: 30
-            }
-          };
+    ionViewDidLoad(){
+        this.loadMap();
+      }
+     
+      loadMap(){
+     
+        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+     
+        let mapOptions = {
+          center: latLng,
+          zoom: 15
+        }
+     
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+     
+      }
 
-        this.mapElement = document.getElementById('map');
-        this.zone.run(() => {
-            this.map = this.googleMaps.create(this.mapElement, mapOptions);          
-            // Wait the MAP_READY before using any methods.
-               this.map.one(GoogleMapsEvent.MAP_READY)
-               .then(() => {
-                 console.log('Map is ready!');
-                 this.changeDetectorRefs.forEach((detector) => {
-                    detector.detectChanges();
-                  });
-               });
-        });
-    }
     logout() {
         localStorage.removeItem('TestToken');
         this._navCtrl.navigateUnprotected('clear');
--- src/pages/markers/markers.ts
+++ src/pages/markers/markers.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
-import { ModalController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+import { FriendsService } from '../friends/friends-service';
 import { MarkersService } from './markers-service';
-
 @Component({
   selector: 'markers-page',
   templateUrl: 'markers.html'
 })
 export class MarkersPage implements OnInit{
+
   markers: any;
-  constructor( public modalCtrl: ModalController, public _markersService: MarkersService, public zone: NgZone ) {}
+  constructor( public alertCtrl: AlertController, public _markersService: MarkersService, public _friendsService: FriendsService ) {}
   
   ngOnInit() {
       this._markersService.getMarkers().subscribe( res => {
@@ -16,4 +17,51 @@ export class MarkersPage implements OnInit{
       });
   }
 
+  shareMarker( marker ) {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Share with friends');
+    
+    let friends = this._friendsService.getFriends();
+
+    for ( let friend of friends ) {
+      alert.addInput({
+        type: 'checkbox',
+        label: friend.name,
+        value: friend.id.toString()
+      });
+    }
+
+
+    alert.addButton({
+      text: 'Okay',
+      handler: data => {
+        console.log('Checkbox data:', data);
+      }
+    });
+
+    alert.present();
+  }
+
+  deleteMarker( marker ) {
+    let confirm = this.alertCtrl.create({
+      title: 'Delete this marker?',
+      message: "This will remove the marker from all who you've shared it with.",
+      buttons: [
+        {
+          text: 'Nah, keep it',
+          handler: () => {
+            console.log('Keep clicked');
+          }
+        },
+        {
+          text: 'Delete',
+          cssClass: 'deleteButton',
+          handler: () => {
+            console.log('Delete clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
